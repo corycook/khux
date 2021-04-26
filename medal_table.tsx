a32e02e647fa63d2914e2bd41f5ecdf07b2272c6
Cory Cook <2746394+corycook@users.noreply.github.com>
2021-04-25 19:15:21 -0700

    Add more columns multiplier, attribute, gauge, etc

--- medal_table.tsx
+++ medal_table.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { DataGrid, ColDef, ValueGetterParams, GridCellParams } from '@material-ui/data-grid';
+import { DataGrid, GridCellParams, GridValueGetterParams } from '@material-ui/data-grid';
 import * as medals from './medals.json';
 import { Tooltip, createStyles, makeStyles, Theme } from '@material-ui/core';
 
@@ -16,6 +16,8 @@ const rows = Array.from({ ...medals, length: 2100 })
   .map(item => ({
     ...item,
     id: item.ID,
+    abilityText: item.Ability?.Text,
+    abilityCondition: 'Condition' in item.Ability ? item.Ability.Condition : undefined,
   }));
 
 export function MedalTable() {
@@ -23,27 +25,56 @@ export function MedalTable() {
 
   return (
     <div style={{ height: '100%', width: '100%' }}>
-      <DataGrid rows={rows} autoHeight columns={[
-        {
-          field: 'MedalImage',
-          headerName: 'MedalImage',
-          flex: 0,
-          renderCell: (params: GridCellParams) => (
-            <Tooltip placement="right" arrow classes={{ tooltip: classes.noMaxWidth }}
-              title={<img src={`/images/medals/en/${params.value}`} width="300" />}>
-              <img src={`/images/medals/en/${params.value}`} width="75" />
-            </Tooltip>
-          ),
-          sortable: false,
-          filterable: false,
-        },
-        { field: 'ID', headerName: 'ID', flex: 0, type: 'number' },
-        { field: 'Name', headerName: 'Name', flex: 1 },
-        { field: 'Rarity', headerName: 'Rarity', flex: 0, type: 'number' },
-        { field: 'Direction', headerName: 'Direction', flex: 1, },
-        { field: 'STR', headerName: 'STR', flex: 0, type: 'number' },
-        { field: 'DEF', headerName: 'DEF', flex: 0, type: 'number', },
-      ]} pageSize={50} />
+      <DataGrid
+        pageSize={50}
+        sortModel={[{ field: 'id', sort: 'desc' }]}
+        rows={rows}
+        autoHeight
+        columns={[
+          {
+            field: 'MedalImage',
+            flex: 0,
+            renderCell: (params: GridCellParams) => (
+              <Tooltip placement="right" arrow classes={{ tooltip: classes.noMaxWidth }}
+                title={<img src={`/images/medals/en/${params.value}`} width="300" />}>
+                <img src={`/images/medals/en/${params.value}`} width="75" />
+              </Tooltip>
+            ),
+            sortable: false,
+            filterable: false,
+          },
+          { field: 'id', flex: 0, type: 'number' },
+          { field: 'Name', flex: 1 },
+          { field: 'Rarity', flex: 0, type: 'number', hide: true },
+          { field: 'Direction', flex: 0 },
+          { field: 'Attribute', flex: 0 },
+          { field: 'Guilt', flex: 0, type: 'number', hide: true },
+          { field: 'STR', flex: 0, type: 'number' },
+          { field: 'DEF', flex: 0, type: 'number', hide: true },
+          { field: 'Multi', headerName: 'Multiplier', flex: 0, type: 'number', hide: true },
+          { field: 'LowMulti', headerName: 'Low Multiplier', flex: 0, type: 'number', hide: true },
+          { field: 'HighMulti', headerName: 'High Multiplier', flex: 0, type: 'number', hide: true },
+          { field: 'abilityCondition', headerName: 'Multiplier Condition', flex: 0, hide: true },
+          {
+            field: 'MultiAll',
+            headerName: 'Multiplier (all)',
+            valueGetter: (params: GridValueGetterParams) => (params.getValue('Multi')
+              || `${params.getValue('LowMulti')} - ${params.getValue('HighMulti')} (${params.getValue('abilityCondition')})`),
+            renderCell: overflowCell,
+            flex: 0
+          },
+          { field: 'Gauge', flex: 0, type: 'number' },
+          { field: 'Target', flex: 0 },
+          { field: 'abilityText', headerName: 'Ability Text', flex: 1, renderCell: overflowCell },
+        ]} />
     </div>
+  );
+}
+
+function overflowCell(params: GridCellParams) {
+  return (
+    <Tooltip arrow title={params.value}>
+      <span>{params.value}</span>
+    </Tooltip>
   );
 }
