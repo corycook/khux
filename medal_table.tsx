14301775636dc43d18b7939da1afa09d330ae65d
Cory Cook <2746394+corycook@users.noreply.github.com>
2021-04-27 08:43:11 +0000

    Use images in table cells.

--- medal_table.tsx
+++ medal_table.tsx
@@ -15,7 +15,7 @@ const rows = Array.from({ ...medals, length: 2100 })
   .filter(item => !!item)
   .map(item => ({
     ...item,
-    id: item.ID,
+    id: item.AlbumNum,
     abilityText: item.Ability?.Text,
     abilityCondition: 'Condition' in item.Ability ? item.Ability.Condition : undefined,
   }));
@@ -28,7 +28,9 @@ export function MedalTable() {
       <div style={{ flexGrow: 1 }}>
         <DataGrid
           pageSize={50}
-          sortModel={[{ field: 'id', sort: 'desc' }]}
+          sortModel={[{ field: 'AlbumNum', sort: 'desc' }]}
+          density="comfortable"
+          disableColumnFilter={false}
           rows={rows}
           columns={[
             {
@@ -37,18 +39,57 @@ export function MedalTable() {
               renderCell: (params: GridCellParams) => (
                 <Tooltip placement="right" arrow classes={{ tooltip: classes.noMaxWidth }}
                   title={<img src={`images/medals/${params.value}`} width="300" />}>
-                  <img src={`images/medals/${params.value}`} width="75" />
+                  <img src={`images/medals/${params.value}`} height="50" />
                 </Tooltip>
               ),
+              renderHeader: () => <i></i>,
               sortable: false,
               filterable: false,
             },
-            { field: 'id', flex: 0, type: 'number' },
+            { field: 'ID', headerName: 'Release Number', flex: 0, type: 'number', hide: true },
+            { field: 'AlbumNum', headerName: 'Album Number', flex: 0, type: 'number' },
             { field: 'Name', flex: 1 },
             { field: 'Rarity', flex: 0, type: 'number', hide: true },
-            { field: 'Direction', flex: 0 },
-            { field: 'Attribute', flex: 0 },
-            { field: 'Guilt', flex: 0, type: 'number', hide: true },
+            {
+              field: 'Direction',
+              flex: 0,
+              renderCell: (params: GridCellParams) => params.value && (
+                <Tooltip arrow placement="bottom" title={`Direction: ${params.value}`}>
+                  <div style={{
+                    alignItems: 'center',
+                    backgroundSize: '100px',
+                    backgroundImage: params.value == 'Upright' 
+                      ? 'url("images/icons/genUp.png")' 
+                      : 'url("images/icons/genRev.png")',
+                    backgroundPosition: 'center',
+                    display: 'flex',
+                    height: '50px',
+                    width: '50px',
+                  }}></div>
+                </Tooltip>
+              ),
+            },
+            {
+              field: 'Attribute',
+              flex: 0,
+              renderCell: (params) => (params.value && params.value !== 'None') ? (
+                <Tooltip arrow title={`Attribute: ${params.value}`}>
+                  <img src={`images/icons/Element${params.value}.png`}
+                    height="50" alt={params.value.toString()} />
+                </Tooltip>
+              ) : null,
+            },
+            {
+              field: 'Guilt',
+              flex: 0,
+              hide: true,
+              renderCell: (params) => params.value && (
+                <Tooltip arrow title={`Guilt: ${params.value}`}>
+                  <img src={`images/icons/Guilt${params.value}.png`} height="50" />
+                </Tooltip>
+              ),
+              type: 'number',
+            },
             { field: 'STR', flex: 0, type: 'number' },
             { field: 'DEF', flex: 0, type: 'number', hide: true },
             { field: 'Multi', headerName: 'Multiplier', flex: 0, type: 'number', hide: true },
@@ -57,19 +98,38 @@ export function MedalTable() {
             { field: 'abilityCondition', headerName: 'Multiplier Condition', flex: 0, hide: true },
             {
               field: 'MultiAll',
+              flex: 0,
               headerName: 'Multiplier (all)',
+              renderCell: overflowCell,
               valueGetter: (params: GridValueGetterParams) => (params.getValue('Multi')
                 || `${params.getValue('LowMulti')} - ${params.getValue('HighMulti')} (${params.getValue('abilityCondition')})`),
-              renderCell: overflowCell,
-              flex: 0
             },
             { field: 'Gauge', flex: 0, type: 'number' },
-            { field: 'Target', flex: 0 },
+            {
+              field: 'Target',
+              flex: 0,
+              renderCell: (params) => params.value && (
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <img src={`images/icons/Target_${params.value}.png`} height="25" />&nbsp;{params.value}
+                </div>
+              ),
+            },
             { field: 'abilityText', headerName: 'Ability Text', flex: 1, renderCell: overflowCell },
           ]} />
       </div>
     </div>
   );
+}
+
+function DirectionCell({ value }) {
+  switch (value) {
+    case 'Upright':
+      return <img src="images/icons/genUp.png" height="100" />;
+    case 'Reversed':
+      return <img src="images/icons/genRev.png" height="100" />;
+    default:
+      return <></>;
+  }
 }
 
 function overflowCell(params: GridCellParams) {
