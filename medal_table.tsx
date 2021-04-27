188adfe5b38254f3749816d86b31125bd0d3dc71
Cory Cook <2746394+corycook@users.noreply.github.com>
2021-04-27 03:58:25 +0000

    Fix table style to fit in the page

--- medal_table.tsx
+++ medal_table.tsx
@@ -24,56 +24,61 @@ export function MedalTable() {
   const classes = useStyles();
 
   return (
-    <div style={{ height: '100%', width: '100%' }}>
-      <DataGrid
-        pageSize={50}
-        sortModel={[{ field: 'id', sort: 'desc' }]}
-        rows={rows}
-        autoHeight
-        columns={[
-          {
-            field: 'MedalImage',
-            flex: 0,
-            renderCell: (params: GridCellParams) => (
-              <Tooltip placement="right" arrow classes={{ tooltip: classes.noMaxWidth }}
-                title={<img src={`images/medals/${params.value}`} width="300" />}>
-                <img src={`images/medals/${params.value}`} width="75" />
-              </Tooltip>
-            ),
-            sortable: false,
-            filterable: false,
-          },
-          { field: 'id', flex: 0, type: 'number' },
-          { field: 'Name', flex: 1 },
-          { field: 'Rarity', flex: 0, type: 'number', hide: true },
-          { field: 'Direction', flex: 0 },
-          { field: 'Attribute', flex: 0 },
-          { field: 'Guilt', flex: 0, type: 'number', hide: true },
-          { field: 'STR', flex: 0, type: 'number' },
-          { field: 'DEF', flex: 0, type: 'number', hide: true },
-          { field: 'Multi', headerName: 'Multiplier', flex: 0, type: 'number', hide: true },
-          { field: 'LowMulti', headerName: 'Low Multiplier', flex: 0, type: 'number', hide: true },
-          { field: 'HighMulti', headerName: 'High Multiplier', flex: 0, type: 'number', hide: true },
-          { field: 'abilityCondition', headerName: 'Multiplier Condition', flex: 0, hide: true },
-          {
-            field: 'MultiAll',
-            headerName: 'Multiplier (all)',
-            valueGetter: (params: GridValueGetterParams) => (params.getValue('Multi')
-              || `${params.getValue('LowMulti')} - ${params.getValue('HighMulti')} (${params.getValue('abilityCondition')})`),
-            renderCell: overflowCell,
-            flex: 0
-          },
-          { field: 'Gauge', flex: 0, type: 'number' },
-          { field: 'Target', flex: 0 },
-          { field: 'abilityText', headerName: 'Ability Text', flex: 1, renderCell: overflowCell },
-        ]} />
+    <div style={{ display: 'flex', height: '100%' }}>
+      <div style={{ flexGrow: 1 }}>
+        <DataGrid
+          pageSize={50}
+          sortModel={[{ field: 'id', sort: 'desc' }]}
+          rows={rows}
+          autoHeight
+          columns={[
+            {
+              field: 'MedalImage',
+              flex: 0,
+              renderCell: (params: GridCellParams) => (
+                <Tooltip placement="right" arrow classes={{ tooltip: classes.noMaxWidth }}
+                  title={<img src={`images/medals/${params.value}`} width="300" />}>
+                  <img src={`images/medals/${params.value}`} width="75" />
+                </Tooltip>
+              ),
+              sortable: false,
+              filterable: false,
+            },
+            { field: 'id', flex: 0, type: 'number' },
+            { field: 'Name', flex: 1 },
+            { field: 'Rarity', flex: 0, type: 'number', hide: true },
+            { field: 'Direction', flex: 0 },
+            { field: 'Attribute', flex: 0 },
+            { field: 'Guilt', flex: 0, type: 'number', hide: true },
+            { field: 'STR', flex: 0, type: 'number' },
+            { field: 'DEF', flex: 0, type: 'number', hide: true },
+            { field: 'Multi', headerName: 'Multiplier', flex: 0, type: 'number', hide: true },
+            { field: 'LowMulti', headerName: 'Low Multiplier', flex: 0, type: 'number', hide: true },
+            { field: 'HighMulti', headerName: 'High Multiplier', flex: 0, type: 'number', hide: true },
+            { field: 'abilityCondition', headerName: 'Multiplier Condition', flex: 0, hide: true },
+            {
+              field: 'MultiAll',
+              headerName: 'Multiplier (all)',
+              valueGetter: (params: GridValueGetterParams) => (params.getValue('Multi')
+                || `${params.getValue('LowMulti')} - ${params.getValue('HighMulti')} (${params.getValue('abilityCondition')})`),
+              renderCell: overflowCell,
+              flex: 0
+            },
+            { field: 'Gauge', flex: 0, type: 'number' },
+            { field: 'Target', flex: 0 },
+            { field: 'abilityText', headerName: 'Ability Text', flex: 1, renderCell: overflowCell },
+          ]} />
+      </div>
     </div>
   );
 }
 
 function overflowCell(params: GridCellParams) {
   return (
-    <Tooltip arrow title={params.value}>
+    <Tooltip
+      arrow
+      style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}
+      title={params.value}>
       <span>{params.value}</span>
     </Tooltip>
   );
